6444af91bfccc028c16d6d3a10b37a0445bd3961
Vitaliy Viktorovich <po4tavela@gmail.com>
2023-11-14 11:11:42 +0300

    Добавлены секции участники и рассказчики.

--- SwiftUIReusableView/Presentation/PersonList/PersonListView.swift
+++ SwiftUIReusableView/Presentation/PersonList/PersonListView.swift
@@ -9,11 +9,23 @@ import SwiftUI
 
 struct PersonListView: View {
     @State private var people: [Person] = Person.samples
+    @State private var participants = Person.samples.filter { $0.isParticipant }
     
     var body: some View {
         NavigationStack {
             List(people) { person in
-                PersonView(person)
+                Section("person.list.participants.section.title") {
+                    ForEach(participants) { person in
+                        PersonView(person)
+                            .avatarImageShape(.rectangle)
+                    }
+                }
+                Section("person.list.speakers.section.title") {
+                    ForEach(people) { person in
+                        PersonView(person)
+                            .avatarImageShape(.round)
+                    }
+                }
             }
             .navigationTitle(ScreenTitles.personList.localized())
             .navigationBarTitleDisplayMode(.automatic)
