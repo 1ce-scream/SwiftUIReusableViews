ea72bc8a80d8936abe0e3d69767c5bab25dac801
Vitaliy Viktorovich <po4tavela@gmail.com>
2023-11-14 10:02:10 +0300

    Git flow, start project.

---
+++ SwiftUIReusableView/SwiftUIReusableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIReusableViewApp.swift
+//  SwiftUIReusableView
+//
+//  Created by Vitaliy Talalay on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIReusableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
